4a4b396dc5ecb79587136737ca5e4f204e982a1e
Elliott Brooks <21270878+elliette@users.noreply.github.com>
2026-05-21 11:01:45 -0700

    Create initial gen_defaults script that uses part files

---
+++ packages/material_ui/tool/m3e_gen_defaults/bin/gen_defaults.dart
@@ -0,0 +1,22 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// ## Usage
+//
+// Run this program from the root of the git repository.
+//
+// ```
+// dart packages/material_ui/tool/m3e_gen_defaults/bin/gen_defaults.dart
+// ```
+
+import 'dart:io';
+
+// TODO: Import template files.
+// import '../templates/x_template.dart';
+
+void main(List<String> args) {
+  final bool debug = args.contains('--debug') || args.contains('-d');
+  // TODO: Invoke template generators.
+  // const XTemplate().generateFile(debug: debug);
+}
